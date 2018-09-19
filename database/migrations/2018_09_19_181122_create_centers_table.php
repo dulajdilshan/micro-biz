9aedd10f51800b5d77fbcd80fe40ec444426c0de
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-19 23:42:44 +0530

    Center Model created and migrations defined

---
+++ database/migrations/2018_09_19_181122_create_centers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCentersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('branch_id');
+            $table->string('center_code');
+            $table->string('center_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centers');
+    }
+}
