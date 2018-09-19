1250625c199d860ca9e3c05409c081c3749db73d
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-20 00:04:36 +0530

    Seeders created- not defined

---
+++ database/seeds/AdminssTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminssTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
