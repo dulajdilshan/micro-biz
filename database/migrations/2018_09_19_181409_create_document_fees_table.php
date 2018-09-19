5288a93592929562174f19635d31c1e9e69ca179
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-19 23:47:08 +0530

    Other Model created and migrations defined

---
+++ database/migrations/2018_09_19_181409_create_document_fees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_fees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('loan_id');
+            $table->unsignedInteger('cashier_id');
+            $table->bigInteger('amount');
+            $table->unsignedInteger('percentage');
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_fees');
+    }
+}
