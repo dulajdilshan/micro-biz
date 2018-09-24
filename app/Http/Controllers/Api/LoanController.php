af4617ec5d7d3c3201e8a953c6cc6ff586d2117b
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-24 21:39:53 +0530

    get all loans

--- app/Http/Controllers/Api/LoanController.php
+++ app/Http/Controllers/Api/LoanController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use \App\Loan;
+use App\Loan;
+use App\Http\Controllers\Controller;
 
 class LoanController extends Controller
 {
