300a31c7617affd80f48fa314de6f56e9e8528df
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-25 10:44:47 +0530

    Customer page mock data and retrieve

--- resources/assets/js/components/DataTable.js
+++ resources/assets/js/components/DataTable.js
@@ -5,15 +5,21 @@ class DataTable extends Component {
     constructor(props) {
         super(props);
     }
+
     render() {
-        let data = [{
-            name: 'Tanner Linsley',
-            age: 26,
-            friend: {
-                name: 'Jason Maurer',
-                age: 23,
-            }
-        }];
+        // let data = [{
+        //     fullName: 'Jodha Akbar',
+        //     nic: '956722345v',
+        //     groupNumber: 3,
+        //     loanNumber:2,
+        //     loanAmount:23000,
+        //     weeklyPayment:21300,
+        //     phoneNumber:'0712345678'
+        //     // friend: {
+        //     //     name: 'Jason Maurer',
+        //     //     age: 23,
+        //     // }
+        // }];
 
         // let columns = [{
         //     Header: 'Name',
@@ -31,13 +37,20 @@ class DataTable extends Component {
         //     accessor: 'friend.age'
         // }];
 
-        let columns = [
-
-        ];
+        // let columns = [
+        //     {Header: 'Name', accessor: 'fullName'},
+        //     {Header: 'NIC', accessor: 'nic'},
+        //     {Header: 'Group Number', accessor: 'groupNumber'},
+        //     {Header: 'Loan number', accessor: 'loanNumber'},
+        //     {Header: 'Loan Amount', accessor: 'loanAmount'},
+        //     {Header: 'Weekly Payment (Amount)', accessor: 'weeklyPayment'},
+        //     {Header: 'Phone number', accessor: 'phoneNumber'},
+        //
+        // ];
 
         return (
             <div>
-                <ReactTable data={data} columns={columns} className="-striped -highlight" filterable/>
+                <ReactTable data={this.props.data} columns={this.props.columns} className="-striped -highlight" filterable/>
             </div>
         );
     }
--- resources/assets/js/components/common/CustomersPage.js
+++ resources/assets/js/components/common/CustomersPage.js
@@ -8,27 +8,67 @@ export default class CustomersPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedCustomers: [{}, {}]
+            selectedCustomers: [{}, {}],
+            customerTable: {
+                columns: [
+                    {Header: 'Name', accessor: 'fullName'},
+                    {Header: 'NIC', accessor: 'nic'},
+                    {Header: 'Group Number', accessor: 'groupNumber'},
+                    {Header: 'Loan number', accessor: 'loanNumber'},
+                    {Header: 'Loan Amount', accessor: 'loanAmount'},
+                    {Header: 'Weekly Payment (Amount)', accessor: 'weeklyPayment'},
+                    {Header: 'Phone number', accessor: 'phoneNumber'},
+
+                ], data: [{
+                    fullName: 'Jodha Akbar',
+                    nic: '956722345v',
+                    groupNumber: 3,
+                    loanNumber: 2,
+                    loanAmount: 23000,
+                    weeklyPayment: 21300,
+                    phoneNumber: '0712345678'
+                }]
+            }
+
         };
     }
 
     render() {
+        let columns = [
+            {Header: 'Name', accessor: 'fullName'},
+            {Header: 'NIC', accessor: 'nic'},
+            {Header: 'Group Number', accessor: 'groupNumber'},
+            {Header: 'Loan number', accessor: 'loanNumber'},
+            {Header: 'Loan Amount', accessor: 'loanAmount'},
+            {Header: 'Weekly Payment (Amount)', accessor: 'weeklyPayment'},
+            {Header: 'Phone number', accessor: 'phoneNumber'},
+
+        ];
+        let data = [{
+            fullName: 'Jodha Akbar',
+            nic: '956722345v',
+            groupNumber: 3,
+            loanNumber: 2,
+            loanAmount: 23000,
+            weeklyPayment: 21300,
+            phoneNumber: '0712345678'
+        }];
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-2">
+                    <div className="col-sm-3">
                         <button type="button" className="btn btn-secondary btn-lg" data-toggle="modal"
                                 data-target="#customerForm">New Customer
                         </button>
                     </div>
-                    <div className="col-sm-2">
+                    <div className="col-sm-3">
                         <button type="button" className="btn btn-outline-primary btn-lg">
                             Create a group ({this.state.selectedCustomers.length})
                         </button>
                     </div>
                 </div>
                 <br/>
-                <DataTable/>
+                <DataTable columns={this.state.customerTable.columns} data={this.state.customerTable.data}/>
                 <CustomerForm/>
             </div>
         );
