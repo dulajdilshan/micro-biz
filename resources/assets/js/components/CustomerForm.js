4663b8349b289726b29d4203527aeaa7f62505b8
dulajdilshan <balasuriyadulaj@gmail.com>
2018-09-26 15:08:20 +0530

    Add new customer Form Form data added to the  state of the parent

--- resources/assets/js/components/CustomerForm.js
+++ resources/assets/js/components/CustomerForm.js
@@ -1,6 +1,77 @@
 import React, {Component} from 'react';
 
 class CustomerForm extends Component {
+    constructor(props) {
+        super(props);
+        this.initialState = {
+            customer: {
+                first_name: '',
+                last_name: '',
+                age: '',
+                nic: '',
+                gender: 'male',
+                married: 'single',
+                contact_no1: '',
+                contact_no2: '',
+                address_1: '',
+                address_2: ''
+            }
+        };
+        this.state = this.initialState;
+    }
+
+    handleNicChange(event) {
+        this.props._handleNicChange(event);
+    }
+
+    handleFirstNameChange(event) {
+        this.props._handleFirstNameChange(event);
+    }
+
+    handleLastNameChange(event) {
+        this.props._handleLastNameChange(event);
+    }
+
+    handleAgeChange(event) {
+        this.props._handleAgeChange(event);
+    }
+
+    handleBirthdayChange(event) {
+        this.props._handleBirthdayChange(event);
+    }
+
+    handleAddress1Change(event) {
+        this.props._handleAddress1Change(event);
+    }
+
+    handleAddress2Change(event) {
+        this.props._handleAddress2Change(event);
+    }
+
+    handleContactNo1Change(event) {
+        this.props._handleContactNo1Change(event);
+    }
+
+    handleContactNo2Change(event) {
+        this.props._handleContactNo2Change(event);
+    }
+
+    handleGenderChange(event) {
+        this.props._handleGenderChange(event);
+    }
+
+    handleMarriedSingleChange(event) {
+        this.props._handleMarriedSingleChange(event);
+    }
+
+    handleCloseButton(event) {
+        //Close the Form button
+    }
+
+    handleAddCustomerButton(event) {
+        //Add new Customer Button
+    }
+
     render() {
         return (
             <div className="modal fade" id="customerForm" role="dialog">
@@ -15,69 +86,109 @@ class CustomerForm extends Component {
                                 <div className="row">
                                     <div className="col-sm-2 form-group">
                                         <label for="customer_nic"> NIC</label>
-                                        <input type="text" className="form-control" id="customer_nic"
-                                               name="customer_nic" required="" maxlength="50"/>
+                                        <input type="text" className="form-control" id="nic"
+                                               name="customer_nic" required="" maxlength="50"
+                                               value={this.props.newCustomer.nic}
+                                               onChange={(event) => {
+                                                   this.handleNicChange(event);
+                                               }}
+                                        />
                                     </div>
-                                    <div className="col-sm-5 form-group">
-                                        <label for="customer_name"> Name</label>
-                                        <input type="text" className="form-control" id="customer_name"
-                                               name="customer_name" required="" maxlength="50"/>
+                                    <div className="col-sm-2 form-group">
+                                        <label for="customer_name"> First Name</label>
+                                        <input type="text" className="form-control" id="first_name"
+                                               name="customer_name" required="" maxlength="50"
+                                               value={this.props.newCustomer.first_name}
+                                               onChange={this.handleFirstNameChange.bind(this)}
+                                        />
+                                    </div>
+                                    <div className="col-sm-3 form-group">
+                                        <label htmlFor="customer_name"> Last Name</label>
+                                        <input type="text" className="form-control" id="last_name"
+                                               name="customer_name" required="" maxLength="50"
+                                               value={this.props.newCustomer.last_name}
+                                               onChange={this.handleLastNameChange.bind(this)}
+                                        />
                                     </div>
                                     <div className="col-sm-3 form-group">
                                         <label for="customer_bday"> Birthday</label>
-                                        <input type="text" className="form-control" id="customer_bday"
-                                               name="customer_bday" required="" maxlength="50"/>
+                                        <input type="text" className="form-control" id="birthday"
+                                               name="customer_bday" required="" maxlength="50"
+                                               value={this.props.newCustomer.birthday}
+                                               onChange={this.handleBirthdayChange.bind(this)}
+                                        />
                                     </div>
                                     <div className="col-sm-2 form-group">
                                         <label for="customer_age"> Age</label>
-                                        <input type="text" className="form-control" id="customer_age"
-                                               name="customer_age" required="" maxlength="50"/>
+                                        <input type="text" className="form-control" id="age"
+                                               name="customer_age" required="" maxlength="50"
+                                               value={this.props.newCustomer.age}
+                                               onChange={this.handleAgeChange.bind(this)}
+                                        />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-sm-3 form-group">
                                         <label for="customer_gender"> Gender</label>
-                                        <select className="form-control" name="customer_gender"
-                                                id="customer_gender">
-                                            <option className="form-control" value="male">Male</option>
-                                            <option className="form-control" value="Female">Female</option>
+                                        <select className="form-control" name="gender"
+                                                id="customer_gender" onChange={this.handleGenderChange.bind(this)}
+                                                value={this.props.newCustomer.gender}>
+                                            <option className="form-control" id='male' value="male">Male</option>
+                                            <option className="form-control" id='female' value="female">Female</option>
                                         </select>
                                     </div>
                                     <div className="col-sm-3 form-group">
                                         <label for="customer_married"> Married / Single</label>
-                                        <select className="form-control" name="customer_married"
-                                                id="customer_married">
+                                        <select className="form-control" name="married"
+                                                value={this.props.newCustomer.married}
+                                                id="customer_married"
+                                                onChange={this.handleMarriedSingleChange.bind(this)}
+                                        >
                                             <option className="form-control" value="married">Married</option>
                                             <option className="form-control" value="single">Single</option>
                                         </select>
                                     </div>
                                     <div className="col-sm-3 form-group">
                                         <label for="customer_contact1"> Phone no</label>
-                                        <input type="text" className="form-control" id="customer_contact1"
-                                               name="customer_contact1" required="" maxlength="50"/>
+                                        <input type="text" className="form-control" id="contact_no1"
+                                               name="customer_contact1" required="" maxlength="50"
+                                               value={this.props.newCustomer.contact_no1}
+                                               onChange={this.handleContactNo1Change.bind(this)}
+                                        />
                                     </div>
                                     <div className="col-sm-3 form-group">
                                         <label for="customer_contact2"> Mobile no</label>
-                                        <input type="text" className="form-control" id="customer_contact2"
-                                               name="customer_contact2" required="" maxlength="50"/>
+                                        <input type="text" className="form-control" id="contact_no2"
+                                               name="customer_contact2" required="" maxlength="50"
+                                               value={this.props.newCustomer.contact_no2}
+                                               onChange={this.handleContactNo2Change.bind(this)}
+                                        />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-sm-6 form-group">
                                         <label for="customer_address1"> Address 1</label>
-                                        <input type="text" className="form-control" id="customer_address1"
-                                               name="customer_address1" required="" maxlength="70"/>
+                                        <input type="text" className="form-control" id="address_1"
+                                               name="customer_address1" required="" maxlength="70"
+                                               value={this.props.newCustomer.address_1}
+                                               onChange={this.handleAddress1Change.bind(this)}
+                                        />
                                     </div>
                                     <div className="col-sm-6 form-group">
                                         <label for="customer_address1"> Address 2</label>
-                                        <input type="text" className="form-control" id="customer_address2"
-                                               name="customer_address2" required="" maxlength="70"/>
+                                        <input type="text" className="form-control" id="address_2"
+                                               name="customer_address2" required="" maxlength="70"
+                                               value={this.props.newCustomer.address_2}
+                                               onChange={this.handleAddress2Change.bind(this)}
+                                        />
                                     </div>
                                 </div>
                             </div>
                             <div className="modal-footer">
-                                <button type="submit" className="btn btn-primary">ADD</button>
-                                <button type="reset" className="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="submit" className="btn btn-primary">Add</button>
+                                <button className="btn btn-default" data-dismiss="modal"
+                                        onClick={this.handleCloseButton.bind(this)}>Close
+                                </button>
                             </div>
                         </form>
                     </div>
--- resources/assets/js/components/common/CustomersPage.js
+++ resources/assets/js/components/common/CustomersPage.js
@@ -9,10 +9,25 @@ import axios from "axios/index";
 export default class CustomersPage extends Component {
     constructor(props) {
         super(props);
+        this.initialState = {
+            customer: {
+                first_name: '',
+                last_name: '',
+                age: '',
+                nic: '',
+                gender: 'male',
+                married: 'single',
+                contact_no1: '',
+                contact_no2: '',
+                address_1: '',
+                address_2: ''
+            }
+        };
         this.state = {
-            newCustomer:{},
+            isFormVisible: false,
+            customer: this.initialState.customer,
             selectedCustomers: [{}, {}],
-            customerList:[],
+            customerList: [],
             customerTable: {
                 columns: [
                     {Header: 'Name', accessor: 'full_name'},
@@ -37,7 +52,102 @@ export default class CustomersPage extends Component {
         };
     }
 
-    componentDidMount(){
+    _handleNicChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {nic: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleFirstNameChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {first_name: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleLastNameChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {last_name: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleAgeChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {age: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleBirthdayChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {birthday: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleAddress1Change(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {address_1: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleAddress2Change(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {address_2: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleContactNo1Change(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {contact_no1: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleContactNo2Change(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {contact_no2: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleGenderChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {gender: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleMarriedSingleChange(event) {
+        let newCustomer = Object.assign({}, this.state.customer, {married: event.target.value});
+        this.setState({
+            customer: newCustomer
+        });
+    }
+
+    _handleCloseButton(event) {
+        console.log(this.state);
+        this.setState(this.initialState);
+        console.log(this.state);
+    }
+
+    _resetNewCustomerForm() {
+        this.setState(this.initialState);
+    }
+
+    _makeNewCustomerFormVisible() {
+        this.setState({isFormVisible: true});
+    }
+
+    _makeNewCustomerFormInvisible() {
+        this.setState({isFormVisible: false});
+    }
+
+    componentDidMount() {
         axios.get('/api/customers')
             .then(res => {
                 this.setState({customerList: res.data});
@@ -74,7 +184,8 @@ export default class CustomersPage extends Component {
                 <div className="row">
                     <div className="col-sm-3">
                         <button type="button" className="btn btn-secondary btn-lg" data-toggle="modal"
-                                data-target="#customerForm">New Customer
+                                data-target="#customerForm" onClick={this._resetNewCustomerForm.bind(this)}>New
+                            Customer
                         </button>
                     </div>
                     <div className="col-sm-3">
@@ -85,7 +196,21 @@ export default class CustomersPage extends Component {
                 </div>
                 <br/>
                 <DataTable columns={this.state.customerTable.columns} data={this.state.customerList}/>
-                <CustomerForm/>
+                <CustomerForm newCustomer={this.state.customer}
+                              isVisible={this.state.isFormVisible}
+                              _handleNicChange={this._handleNicChange.bind(this)}
+                              makeInvisible={this._makeNewCustomerFormInvisible}
+                              _handleFirstNameChange={this._handleFirstNameChange.bind(this)}
+                              _handleLastNameChange={this._handleLastNameChange.bind(this)}
+                              _handleAgeChange={this._handleAgeChange.bind(this)}
+                              _handleBirthdayChange={this._handleBirthdayChange.bind(this)}
+                              _handleAddress1Change={this._handleAddress1Change.bind(this)}
+                              _handleAddress2Change={this._handleAddress2Change.bind(this)}
+                              _handleContactNo1Change={this._handleContactNo1Change.bind(this)}
+                              _handleContactNo2Change={this._handleContactNo2Change.bind(this)}
+                              _handleGenderChange={this._handleGenderChange.bind(this)}
+                              _handleMarriedSingleChange={this._handleMarriedSingleChange.bind(this)}
+                />
             </div>
         );
     }
