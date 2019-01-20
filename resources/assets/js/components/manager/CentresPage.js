d599ac9ca5d2b5eb6c95ab8ea094eab29ef17e04
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-01-21 02:44:10 +0530

    Form Structure declared and button changed to pop up the modal

--- resources/assets/js/components/manager/CentresPage.js
+++ resources/assets/js/components/manager/CentresPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import "react-table/react-table.css";
 import DataTable from "../DataTable";
+import Form from "../Form";
 
 export default class CentresPage extends Component {
     constructor(props) {
@@ -27,15 +28,48 @@ export default class CentresPage extends Component {
     }
 
     render() {
+        const newCenterFormStructure = [
+            [{
+                label: "Branch ID",
+                id: "branch_id",
+                name: "branch_id",
+                required: true,
+                pattern: "^[0-9]+$",
+                message: "Numeric Only",
+                value: this.state.center.branch_id,
+                onChange: () => console.log('typing..'),
+            },{
+                label: "Center Code",
+                id: "center_code",
+                name: "center_code",
+                required: true,
+                pattern: "^[0-9]+$",
+                message: "Numeric Only",
+                value: this.state.center.center_code,
+                onChange: () => console.log('typing..'),
+            },{
+                label: "Center Name",
+                id: "center_name",
+                name: "center_name",
+                required: true,
+                pattern: "^[A-Za-z]+$",
+                message: "invalid",
+                value: this.state.center.center_name,
+                onChange: () => console.log('typing..'),
+            }],
+        ];
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-sm-3">
-                        <button className="btn btn-secondary btn-lg"> New Centre</button>
+                        <button className="btn btn-secondary btn-lg" data-toggle="modal"
+                                data-target="#newCenterForm"> New Centre
+                        </button>
                     </div>
                 </div>
                 <br/>
                 <DataTable columns={this.state.centerTable.columns} data={this.state.dummyCenterTableData}/>
+                <Form name="newCenterForm" title="Add Center"/>
             </div>
         );
     }
