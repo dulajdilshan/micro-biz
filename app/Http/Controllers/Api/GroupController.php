092205507f2fc147da52f280089daa78e8effed1
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-01-27 16:13:43 +0530

    Controller (group) - minor changes

--- app/Http/Controllers/Api/GroupController.php
+++ app/Http/Controllers/Api/GroupController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Branch;
+use App\Customer;
 use App\Group;
 use Illuminate\Http\Request;
+use \Exception;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class GroupController extends Controller
@@ -26,13 +30,34 @@ class GroupController extends Controller
      */
     public function create(Request $request)
     {
-        return response()->json($request);
+        DB::beginTransaction();
+        try {
+            $branch = Branch::where('id', $request['branch_id'])->first();
+            if ($branch==null){
+                return response('Error');
+            }
+        }catch (Exception $e){
+            DB::rollBack();
+            return redirect('manager-groups');
+        }
+        
+
+        //Save the Created Group
+        $group = new Group();
+        $ungrouped_customers = Customer::where('group_id', 0)->get();
+
+        $customer1 = $request['selectedCustomers']['customer_1'];
+        $customer2 = $request['selectedCustomers']['customer_2'];
+        $customer3 = $request['selectedCustomers']['customer_3'];
+        $customer4 = $request['selectedCustomers']['customer_4'];
+        $customer5 = $request['selectedCustomers']['customer_5'];
+        return response()->json($request['selectedCustomers']['customer_1']);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -43,7 +68,7 @@ class GroupController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -54,7 +79,7 @@ class GroupController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -65,8 +90,8 @@ class GroupController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -77,7 +102,7 @@ class GroupController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
