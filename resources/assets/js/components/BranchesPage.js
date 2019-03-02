9a7cd58a85764d08529c0d645fe78c9c9a292be2
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2019-03-02 09:49:51 +0530

    - branches page first commit

---
+++ resources/assets/js/components/BranchesPage.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import "react-table/react-table.css";
+import DataTable from "./DataTable";
+import Form from "./Form";
+import axios from "axios";
+
+export default class BranchesPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log("Hi im here");
+        return (
+            <div className="container">
+                <div className="row">
+                    <h1>This is Branches Page</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+if (document.getElementById('branches-page')) {
+    ReactDOM.render(<BranchesPage/>, document.getElementById('branches-page'));
+}
